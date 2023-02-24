80e74b0237b0f4a7f4313294d907aae60ea9d443
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-02-24 12:42:22 +0530

    Day04

---
+++ Typescript/Day04/demo1.js
@@ -0,0 +1,3 @@
+console.log("Hello World");
+console.log('Welcome to angular 12 module');
+console.log("\n            Hopes so u all r enjoy!!!!!\n");
---
+++ Typescript/Day04/demo1.ts
@@ -0,0 +1,9 @@
+
+console.log("Hello World");
+console.log('Welcome to angular 12 module')
+console.log(`
+            Hopes so u all r enjoy!!!!!
+`);
+
+
+
