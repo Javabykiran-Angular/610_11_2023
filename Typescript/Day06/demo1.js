80305aaa10a84687945a31cf08c681f208ae5088
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-02-28 12:48:58 +0530

    Day06

---
+++ Typescript/Day06/demo1.js
@@ -0,0 +1,52 @@
+//control statement
+// if,if-else,nested if-else,switch,break,continue
+var s1 = 4;
+// if(s1>4){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is False");
+// }
+// if(s1>4){
+//     console.log("Condition is true");
+// }else if(s1==4){
+//     console.log("Condition is equal");
+// }else{
+//     console.log("Condition is False");
+// }
+//switch
+var choice = 21;
+// switch(choice){
+//     case 1:console.log("u r in case 1");
+//             break;
+//     case 2 : console.log("u r in case 2");
+//             break;
+//     default:console.log("u r in default case");
+//             break;
+// }
+//Loop statement
+//while ,do-while,for ,foreach
+var count = 5;
+// while(count!=0){
+//     console.log("Count is "+count);
+//     count--;
+// }
+// do{
+//     console.log("Count is "+count);
+//     count--;
+// }while(count!=0)
+var b1 = 11;
+// do{
+//     console.log("Count is "+count)
+//     count++;
+// }while(count<=10);
+// for loop
+// for(var i=0;i<5;i++){
+//     console.log("value of i "+i)
+// }
+// console.log("Value of i after loop "+i);
+// var => variable has global scope
+//let => it is keyword to create a variable 
+// scope => its has within nearest block ({})
+for (var i = 0; i < 5; i++) {
+    console.log("value of i " + i);
+}
